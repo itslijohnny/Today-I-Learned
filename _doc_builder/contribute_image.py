05be37d579ab62fc2f12619d89c744ad52edf8ff
Johnny Li <johnnyhli@outlook.com>
2024-02-02 21:46:30 -0800

    Debuging contribute_image.py

--- _doc_builder/contribute_image.py
+++ _doc_builder/contribute_image.py
@@ -3,8 +3,10 @@ import calmap
 import matplotlib.pyplot as plt
 import git
 from helper.lib import abs_path, PROJECT_DIR
+from loguru import logger
  
 repo = git.Repo(PROJECT_DIR)
+logger.info(f'project dir {PROJECT_DIR}')
 commits = list(repo.iter_commits(paths='.'))
 data = pd.DataFrame({
     "commit_id": [commit.hexsha for commit in commits],
@@ -15,6 +17,7 @@ data['date'] = pd.to_datetime(data['date'])
 summary = data[['date', 'commit_id']].groupby('date').count().reset_index()
 summary.set_index("date", inplace=True)
 summary["commit_id"]
+logger.info(summary["commit_id"])
 fig, axes = calmap.calendarplot(summary["commit_id"], fig_kws={"figsize": (12, 8)}, cmap="YlGn")
 axes[0].set_title("TIL Updates", fontsize=18)
 plt.savefig(abs_path("assets/til_update.png"), bbox_inches='tight')
