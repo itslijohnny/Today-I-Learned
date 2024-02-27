1d4edb2c051c05a0004d437729acc35c4fe682ee
Johnny Li <5987932+itslijohnny@users.noreply.github.com>
2024-02-26 23:18:00 -0800

    Fix error in creating TIL update image

--- _doc_builder/contribute_image.py
+++ _doc_builder/contribute_image.py
@@ -18,6 +18,11 @@ summary = data[['date', 'commit_id']].groupby('date').count().reset_index()
 summary.set_index("date", inplace=True)
 summary["commit_id"]
 logger.info(summary["commit_id"])
-fig, axes = calmap.calendarplot(summary["commit_id"], fig_kws={"figsize": (12, 8)}, cmap="YlGn")
-axes[0].set_title("TIL Updates", fontsize=18)
-plt.savefig(abs_path("assets/til_update.png"), bbox_inches='tight')
+try:
+    fig, axes = calmap.calendarplot(summary["commit_id"], fig_kws={"figsize": (12, 8)}, cmap="YlGn")
+    axes[0].set_title("TIL Updates", fontsize=18)
+    plt.savefig(abs_path("assets/til_update.png"), bbox_inches='tight')
+except Exception as e:
+    logger.error(e)
+    logger.error('Error in creating the image')
+    pass
